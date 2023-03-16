001370183c8446795c4e40f8433e85947014338a
csjoy <antohin.joy@gmail.com>
2023-03-16 13:55:48 +0600

    enrich alpha

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -8,5 +7,9 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    },
+  })],
 });
