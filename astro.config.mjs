1bd34025297638e899338cb0d78cf2b167166fd3
csjoy <antohin.joy@gmail.com>
2023-03-16 18:21:09 +0600

    project layout change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    },
-  })],
+  integrations: [tailwind()]
 });
